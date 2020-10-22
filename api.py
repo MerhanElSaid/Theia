7d758783a3f89026ed65de48b7b5fca22cc34ff7
Hassan Ibrahim <hassan.abbas3600@gmail.com>
2020-10-22 19:05:49 +0200

    Correct Age Transform

--- api.py
+++ api.py
@@ -73,16 +73,22 @@ def transform_gender_image(image):
 
 def transform_age_image(image):
     img_size = 224
-    faces = np.empty((1, img_size, img_size, 3))
-    faces[0] = image.resize((img_size, img_size))
-    inputs = torch.from_numpy(np.transpose(faces.astype(np.float32), (0, 3, 1, 2))).to('cuda')
+    inputs = None
+    if image.mode == 'L':
+        faces = np.empty((1, img_size, img_size))
+        faces[0] = image.resize((img_size, img_size))
+        inputs = torch.from_numpy(np.transpose(faces.astype(np.float32), (0, 1, 2))).to('cuda').unsqueeze(0).repeat(1, 3, 1, 1)
+    else:
+        faces = np.empty((1, img_size, img_size, 3))
+        faces[0] = image.resize((img_size, img_size))
+        inputs = torch.from_numpy(np.transpose(faces.astype(np.float32), (0, 3, 1, 2))).to('cuda')
     return inputs
 
 
 def transform_facial_exp(image):
     exp_trans = transforms.Compose([
         transforms.Grayscale(num_output_channels=1),
-        transforms.Resize(48),
+        transforms.Resize((48,48)),
         transforms.ToTensor()
     ])
     return exp_trans(image).unsqueeze(0)
